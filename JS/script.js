b1638e12f57a4d5ea575746fcf6d9646c7c3fcb0
Evgenii <zsych888@gmail.com>
2022-02-07 16:17:07 +0200

    Практика ч.1

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,17 @@
-console.log(1);
-let number = 5;
-const leftBorderWidth = 1;
+let numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?', "");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movie: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+let a = prompt('Один из последних просмотреных фильмов', "");
+let b = prompt('Оценка?', "");
+let c = prompt('Один из последних просмотреных фильмов', "");
+let d = prompt('Оценка?', "");
+personalMovieDB.movie[a]=b; 
+personalMovieDB.movie[c]=d; 
+
+console.log(personalMovieDB);
