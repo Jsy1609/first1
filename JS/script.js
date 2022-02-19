d89202c7414ce5a2f5573f6099855d161201cf2d
Evgenii <zsych888@gmail.com>
2022-02-19 20:12:29 +0200

    function

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,14 @@
-let numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?', "");
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?', "");
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?', "");
+    }
+
+}
+
+start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -7,11 +17,49 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
-let a = prompt('Один из последних просмотреных фильмов', "");
-let b = prompt('Оценка?', "");
-let c = prompt('Один из последних просмотреных фильмов', "");
-let d = prompt('Оценка?', "");
-personalMovieDB.movie[a]=b; 
-personalMovieDB.movie[c]=d; 
+let a = "";
+let i = 1;
+function rememberMyFilms() {
 
-console.log(personalMovieDB);
+    while (i <= 2) {
+        do {
+            a = prompt('Один из последних просмотреных фильмов', "");
+            if (a == "") {
+                
+                continue;
+            } /* else if (str.length(a) > 50) {
+                continue;
+            } */;
+        } while (a == "");
+        
+        do {
+            b = prompt('Оценка?', "");
+            if (b == "") {
+                console.log('Вы не ввели Оценкy');
+                continue;
+            };
+        } while (b == "");
+       
+        personalMovieDB.movie[a]=b;
+        i++;
+    };
+}
+
+rememberMyFilms();
+
+function showMyDB() {
+    if (personalMovieDB.privat == false) {
+        console.log(personalMovieDB);
+    } 
+}
+
+function writeYourGenres() {
+    for (let n = 1; n <= 3; n++) {
+    personalMovieDB.genres[n-1] = prompt(`Ваш любимый жанр под номером ${n}`);
+    };
+}
+
+
+writeYourGenres();
+
+showMyDB();
